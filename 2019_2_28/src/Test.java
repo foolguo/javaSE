92d90b5e7401615c6c4aa8a89a7c3fdceeefa3de
郭佳玥 <1103423563@qq.com>
2019-02-28 10:04:22 +0800

    异常处理

---
+++ 2019_2_28/src/Test.java
@@ -0,0 +1,53 @@
+
+/*
+public class Test {
+    public static void main(String[] args) {
+        int a=10,b=0;
+        try{
+            a=a/b;
+            return ;
+        }catch(Exception e) {
+            System.out.println("异常");
+        }finally{
+            System.out.println("一定会执行");
+        }
+    }
+}
+*/
+
+/*public class Test {
+    public static void main(String[] args) throws Exception{
+        int a=10,b=0;*/
+        /*try {*/
+//        System.out.println(print(a,b));
+//        System.out.println("0");
+            /*
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            System.out.println("0");
+        }*/
+//    }
+//    public static int print(int a,int b)throws Exception{
+//        int c=0;
+//        c=a/b;
+//        return c;
+//    }
+//}
+
+/*class AddException extends Exception{
+    public AddException(String msg){
+        super(msg);
+    }
+}
+
+    public class Test{
+        public static void main(String[] args)throws Exception {
+            int a=20,c=30;
+            if(a+c==50){
+                throw new AddException("不能为50");
+            }
+        }
+}*/
+
+
