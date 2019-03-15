fde532c00c4bc53cbea8203b8559de48b5a5ffc4
郭佳玥 <1103423563@qq.com>
2019-03-15 11:23:41 +0800

    线性表

--- 2019_3_14_线性表/src/com/Sequence/Test.java
+++ 2019_3_14_线性表/src/com/Sequence/Test.java
@@ -7,7 +7,7 @@ package com.Sequence;
  */
 public class Test {
     public static void main(String[] args) {
-        DoubleLinkedlist l=new DoubleLinkedlist();
+     /*   DoubleLinkedlist l=new DoubleLinkedlist();
         l.add(1);
         l.add(2);
         l.add(3);
@@ -16,6 +16,27 @@ public class Test {
 
         System.out.println(l.get(1));
         l.remove(1);
-        System.out.println(l.get(1));
+        System.out.println(l.get(1));*/
+      /*  ArrayImpl array=new ArrayImpl();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+        Object[] temp=array.toArray();
+        array.set(2,"abc");
+        for(Object i:temp){
+            System.out.print(i+" ");
+        }*/
+      LinkedList l=new LinkedList();
+      l.add(4);
+      l.add(3);
+      l.add(2);
+      l.add(1);
+      l.remove(1);
+      Object[] temp=l.toArray();
+      for(Object i:temp){
+          System.out.print(i+" ");
+      }
     }
 }
